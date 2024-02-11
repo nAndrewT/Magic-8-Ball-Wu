e72de2f9ad22f9323441947021f39bb2686a50e4
Noah T <noahtofte@gmail.com>
2024-02-11 18:56:51 -0500

    functionality with awful UI

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var ballImage: UIImageView!
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-    @IBAction func askButtonPressed(_ sender: Any) {
-    }
+    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
     
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func askButtonPress(_ sender: UIButton) {
+        imageView.image = ballArray.randomElement()
+    }
 }
 
